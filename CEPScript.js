d6bf3bc73b237395ebf97b75f5b11251a5d872e6
Gabriel Zucchi <gabrielzucchi666@gmail.com>
2025-07-22 10:53:52 -0300

    JSdone

--- CEPScript.js
+++ CEPScript.js
@@ -1,3 +1,33 @@
-const cep = document.getElementById("CEP");
-const botao = document.getElementById("botaoEnviar");
+document.getElementById("CEP").addEventListener("blur", function () {
 
+    const cep = this.value.replace(/\D/g, "");
+
+    if (!(cep.length === 8)) {
+        alert("CEP inválido. Deve conter 8 dígitos.");
+        return;
+    }
+
+    fetch(`https://viacep.com.br/ws/${cep}/json/`)
+        .then(response => {
+            if (!response.ok) throw new Error("Erro ao buscar o CEP");
+            return response.json();
+    })
+
+    .then(data => {
+        if (data.erro) {
+        alert("CEP não encontrado.");
+        return;
+    }
+
+    document.getElementById("logradouro").value = data.logradouro;
+    document.getElementById("municipio").value = data.localidade;
+    document.getElementById("UF").value = data.uf;
+
+    })
+
+    .catch(error => {
+        alert("Erro ao buscar o endereço.");
+        console.error(error);
+    });
+
+});
